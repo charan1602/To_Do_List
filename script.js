61c34ee724975ddefb2eb99d8b04ba873fe9ed22
charan1602 <charanreddy1602@gmail.com>
2024-08-16 20:56:18 +0530

    to do list

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let input = document.getElementById("input")
 let add = document.getElementById("add")
+let title = document.getElementById("title")
+let description = document.getElementById("description")
+let priority = document.getElementById("priority")
+let main = document.getElementById("main_container")
+console.log(main.value)
 
 // ADDING FUNCTION TO ADD BUTTON
 add.addEventListener("click",()=>{
-    let list_memeber = document.createElement("li")
-    list_memeber.innerHTML = `
-     ${input.value}
-    ` 
-    list.appendChild(list_memeber)
+
+    let division = document.createElement("div")
+    division.setAttribute("class","child_div")
+    // console.log(title.value)
+    division.innerHTML = `
+   
+       Title : ${title.value}
+       Description : ${description.value}
+       Priority : ${priority.value} 
+    `
+    // console.log(division)
+    main.appendChild(division)
+    // console.log(division)
 })
